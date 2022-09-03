bde51b0996e7b4b7b130cee42adc2f9d69b17a9c
k <2792580001@qq.com>
2022-09-03 23:57:14 +0800

    first commit

---
+++ src/main/java/k/GitTest.java
@@ -0,0 +1,12 @@
+package k;
+
+/**
+ * @author: K
+ * @date: 2022/9/3 23:53
+ * @description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
